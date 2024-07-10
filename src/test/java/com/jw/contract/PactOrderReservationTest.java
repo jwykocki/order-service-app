f9bb8fac4e42ed2cfd9595e199e18d72262fb67f
Jakub Wykocki <jakubwykocki@outlook.com>
2024-07-10 14:05:15 +0200

    fix test, change ReservationResult to record

--- src/test/java/com/jw/contract/PactOrderReservationTest.java
+++ src/test/java/com/jw/contract/PactOrderReservationTest.java
@@ -7,9 +7,12 @@ import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
 import au.com.dius.pact.consumer.junit5.PactTestFor;
 import au.com.dius.pact.core.model.V4Pact;
 import au.com.dius.pact.core.model.annotations.Pact;
+import com.jw.dto.OrderProductRequest;
+import com.jw.dto.OrderResponse;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.apache.http.HttpHeaders;
 import org.apache.http.HttpStatus;
@@ -17,10 +20,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith(PactConsumerTestExt.class)
-@PactTestFor(providerName = "product_service", hostInterface = "localhost", port = "8082")
+@PactTestFor(providerName = "productService", hostInterface = "localhost", port = "8082")
 public class PactOrderReservationTest {
 
-    @Pact(consumer = "order_service")
+    @Pact(consumer = "orderService")
     public V4Pact createPact(PactDslWithProvider builder) {
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
@@ -40,8 +43,8 @@ public class PactOrderReservationTest {
                                     "quantity": 1
                                 },
                                 {
-                                    "productId": 3,
-                                    "quantity": 1
+                                    "productId": 2,
+                                    "quantity": 2
                                 }
                             ]
                         }
@@ -49,9 +52,10 @@ public class PactOrderReservationTest {
                 .willRespondWith()
                 .status(200)
                 .headers(headers)
-                .body("""
+                .body(
+                        """
                         {
-                            "customerId": 10,
+                            "orderId": 1,
                             "status": "RESERVED",
                             "orderProducts": [
                                 {
@@ -59,8 +63,8 @@ public class PactOrderReservationTest {
                                     "quantity": 1
                                 },
                                 {
-                                    "productId": 3,
-                                    "quantity": 1
+                                    "productId": 2,
+                                    "quantity": 2
                                 }
                             ]
                         }
@@ -84,8 +88,8 @@ public class PactOrderReservationTest {
                                     "quantity": 1
                                 },
                                 {
-                                    "productId": 3,
-                                    "quantity": 1
+                                    "productId": 2,
+                                    "quantity": 2
                                 }
                             ]
                         }
@@ -95,6 +99,13 @@ public class PactOrderReservationTest {
         // then
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.SC_OK);
         assertThat(response.getHeader("Content-Type")).isEqualTo("application/json");
-//        assertThat(response.getBody().asString()).contains("condition", "true", "name", "tom");
+        assertThat(response.getBody().as(OrderResponse.class).orderId()).isNotNull();
+        assertThat(response.getBody().as(OrderResponse.class).status()).isNotNull();
+        List<OrderProductRequest> products =
+                response.getBody().as(OrderResponse.class).orderProducts();
+        assertThat(products).hasSize(2);
+        assertThat(products)
+                .containsExactlyInAnyOrder(
+                        new OrderProductRequest(1L, 1), new OrderProductRequest(2L, 2));
     }
 }
