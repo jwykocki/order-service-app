c95d9a2a4e8e800d1bbb902cea5b0c01c619ae50
Jakub Wykocki <jakubwykocki@outlook.com>
2024-07-13 21:27:57 +0200

    small changes

--- src/main/java/com/jw/dto/OrderResponse.java
+++ src/main/java/com/jw/dto/OrderResponse.java
@@ -1,8 +1,6 @@
 package com.jw.dto;
 
 import java.util.List;
-import lombok.Builder;
 
-@Builder
 public record OrderResponse(
         Long orderId, Long customerId, String status, List<OrderProductRequest> orderProducts) {}
